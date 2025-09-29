1682f9b2142849703688ddadf58f501d0b2dd662
Muffin <loan.roze@efrei.net>
2025-09-29 15:44:52 +0200

    Ajout de toute la logique métier pour users

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,9 @@ const router = express.Router();
 
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
+router.get('/', userController.getUserByEmail);
+router.get('/', userController.login)
+router.post('/', userController.register);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 router.delete('/', userController.deleteAllUsers);
